012ea07594c44fa540fef7b09b71cbcb7554e5f3
Brunozapa <zapabrunosilva@gmail.com>
2021-05-04 15:56:37 -0300

    Create Project

---
+++ Projeto-Santastico/Site/LoginCadastro/ScriptsLC/languages-selector.js
@@ -0,0 +1,13 @@
+function slcIdioma() {
+    var idioma = idIdioma.value;
+
+    if (idioma == 'pt') {
+        window.location.href = "login-pt.html";
+    }
+    if (idioma == 'en') {
+        window.location.href = "login-en.html";
+    }
+    if (idioma == 'es') {
+        window.location.href = "login-es.html";
+    }
+}
